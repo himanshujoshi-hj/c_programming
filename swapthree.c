d6fd14711f385fa5ceb83c343911d4ef6ae8204a
Himanshu Joshi <himanshujoshi1356@gmail.com>
2026-09-20 13:45:14 +0530

    created a new file

---
+++ swapthree.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter number in a:");
+    scanf("%d",&a);10
+    printf("enter number in b:");
+    scanf("%d",&b);
+    c=a;
+    a=b;
+    b=c;
+    printf("after swppping in a=%d\n",a);
+    printf("after swppping in b=%d\n",b);
+
+}
